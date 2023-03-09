a4f257a626b9cf929fdc7cacf172c7355de23d62
bibidi77 <fullmoon7813@gmail.com>
2023-03-09 16:27:16 +0900

    init

---
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/Server001WithsKeyApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.server_001_withskey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server001WithsKeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server001WithsKeyApplication.class, args);
+    }
+
+}
