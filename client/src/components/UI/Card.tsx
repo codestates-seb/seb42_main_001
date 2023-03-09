8c90946dc3cda8a52dc323291f53892e09b48365
dding-v <v.dding.v@gmail.com>
2023-03-09 14:12:28 +0900

    :lipstick: style: Card Component 추가

---
+++ client/src/components/UI/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+interface CardProps {
+    children?:any;
+}
+
+function Card({children} : CardProps) {
+  return <CardLayout>{children}</CardLayout>;
+}
+
+export default Card;
+
+const CardLayout = styled.div`
+    background-color: var(--color-white);
+    box-shadow: 1px 1px 20px rgba(140, 140, 140, 0.4);
+    border-radius: var(--radius-size);
+`;
