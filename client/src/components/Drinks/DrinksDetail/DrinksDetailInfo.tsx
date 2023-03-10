58a06cde05a342aa6deb05a9e4de055d581e54c7
dding-v <v.dding.v@gmail.com>
2023-03-10 10:13:18 +0900

    :lipstick: style: DrinksDetail Info 생성

--- client/src/components/Drinks/DrinksDetail/DrinksDetailInfo.tsx
+++ client/src/components/Drinks/DrinksDetail/DrinksDetailInfo.tsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 function DrinksDetailInfo() {
   return (
     <InfoContainer>
-      DrinksDetailInfo
+      <InfoContainer>Entry-Level</InfoContainer>
+      <InfoContainer>
+        <InfoContainer>
+          <span>도수</span>
+          <span>40%</span>
+        </InfoContainer>
+        <InfoContainer>
+          <span>용량</span>
+          <span>375ml</span>
+        </InfoContainer>
+      </InfoContainer>
     </InfoContainer>
-  )
+  );
 }
 
-export default DrinksDetailInfo
+export default DrinksDetailInfo;
 
 const InfoContainer = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
-`
+  justify-content: space-between;
+`;
