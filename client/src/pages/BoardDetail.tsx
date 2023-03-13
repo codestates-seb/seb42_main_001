816b0dc0d515f37b2be85454c0d033ba2110b8be
hyunhoh <skins0329@gmail.com>
2023-03-13 10:14:18 +0900

    :lipstick: style: Board Detail 페이지 초기 CSS 작업

--- client/src/pages/BoardDetail.tsx
+++ client/src/pages/BoardDetail.tsx
@@ -1,7 +1,32 @@
 import styled from 'styled-components';
 
+import BoardAuthorInfo from '../components/board/BoardAuthorInfo';
+
 function BoardDetail() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <BoardDetailContainer>
+        <BoardAuthorInfo></BoardAuthorInfo>
+      </BoardDetailContainer>
+      <BoardCommentsContainer></BoardCommentsContainer>
+    </Wrapper>
+  );
 }
 
 export default BoardDetail;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const BoardDetailContainer = styled.div`
+  background-color: var(--color-white);
+`;
+
+const BoardCommentsContainer = styled.div`
+  background-color: var(--color-sub-gray);
+`;
