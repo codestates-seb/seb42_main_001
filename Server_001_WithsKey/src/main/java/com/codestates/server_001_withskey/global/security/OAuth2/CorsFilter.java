d12d47d85227d456954f0e7e6775147412812864
bibidi77 <fullmoon7813@gmail.com>
2023-03-18 16:10:46 +0900

    ✨ feat: CORSFilter 구현방식3

--- Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/OAuth2/CorsFilter.java
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/OAuth2/CorsFilter.java
@@ -5,37 +5,43 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
+@WebFilter("/*")
 public class CorsFilter implements Filter {
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException
-    {
-        HttpServletResponse response = (HttpServletResponse) res;
+    public CorsFilter() {
+    }
+
+    @Override
+    public void init(FilterConfig fc) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        System.out.println("doFilter");
+        HttpServletResponse response = (HttpServletResponse) resp;
         HttpServletRequest request = (HttpServletRequest) req;
         response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "*");
-        response.setHeader("Access-Control-Expose-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response
+                .setHeader("Access-Control-Allow-Headers", "Origin, origin, x-requested-with, authorization, " +
+                        "Content-Type, Authorization, credential, X-XSRF-TOKEN");
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
-            chain.doFilter(req, res);
+            chain.doFilter(req, resp);
         }
     }
 
-    public void init(FilterConfig filterConfig) {
-        // not needed
-    }
-
+    @Override
     public void destroy() {
-        //not needed
     }
 }
