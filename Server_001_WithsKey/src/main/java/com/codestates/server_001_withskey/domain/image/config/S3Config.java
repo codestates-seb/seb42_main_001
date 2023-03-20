a2eca425d67d33e0fdc7b1ec794b45b415323187
bibidi77 <fullmoon7813@gmail.com>
2023-03-20 16:24:15 +0900

    ✨ feat: yml 설정변경 및 redis 적용

--- Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/domain/image/config/S3Config.java
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/domain/image/config/S3Config.java
@@ -12,16 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-//    @Value("${cloud.aws.credentials.access-key}")
-    @Value("${AWS.ACCESS-KEY}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-//    @Value("${cloud.aws.credentials.secret-key}")
-    @Value("${AWS.SECRET-KEY}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-//    @Value("${cloud.aws.region.static}")
-    @Value("${AWS-REGION}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
