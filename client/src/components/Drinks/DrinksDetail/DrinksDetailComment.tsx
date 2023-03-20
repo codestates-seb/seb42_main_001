5be3803873a80d34e098be27d4af119804bb79c2
lapmu <antjdgkdl@gmail.com>
2023-03-20 11:32:57 +0900

    ✨ feat: board delete 구현

--- client/src/components/Drinks/DrinksDetail/DrinksDetailComment.tsx
+++ client/src/components/Drinks/DrinksDetail/DrinksDetailComment.tsx
@@ -1,24 +1,16 @@
-import React, { useState } from 'react';
 import styled from "styled-components";
 import Comment from "../../UI/Comment/Comment";
 import CommentInput from "../../UI/Comment/CommentInput";
 import { DrinksDetailProps } from "../../../interfaces/Drinks.inerface";
 
 function DrinksDetailComment({ drinksDetail }: DrinksDetailProps) {
-  const [edit, setEdit] = useState<boolean>(false)
-
-  const handleCommentEditOpen = () => {
-    setEdit(prev => !prev)
-    console.log('prev')
-  }
-
   return (
     <MainContainer>
       <CommentInput drinkId={drinksDetail?.drinkId} />
       {drinksDetail?.commentDrinks.map((el) => {
-        return <Comment key={el.commentId} comments={el} onClick={handleCommentEditOpen} />
+        return <Comment key={el.commentId} comments={el} />;
       })}
-    </MainContainer >
+    </MainContainer>
   );
 }
 
