55f5612dfa4d195abef1a763d49447198d9cc42c
Dvdhan <han2041126@gmail.com>
2023-03-21 13:06:51 +0900

    DV 배포 v1

--- Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/domain/image/config/S3Config.java
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/domain/image/config/S3Config.java
@@ -12,13 +12,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${/main001/s3access/cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${/main001/s3access/cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${/main001/s3access/cloud.aws.region.static}")
     private String region;
 
     @Bean
