8eb96a7734a8a5b3d2e48330ebbe1bf4a149a084
hyunhoh <skins0329@gmail.com>
2023-03-21 20:26:53 +0900

    :sparkles: feat: 새로고침시 로그인 상태 유지 1차 작업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,41 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Main from "./pages/Main";
-import MainLayout from "./components/layout/MainLayout";
-import BaseLayout from "./components/layout/BaseLayout";
-import DrinksList from "./pages/DrinksList";
-import DrinksDetail from "./pages/DrinksDetail";
-import ArticleList from "./pages/ArticleList";
-import MyPage from "./pages/MyPage";
-import BoardList from "./pages/BoardList";
-import BoardDetail from "./pages/BoardDetail";
-import BoardCreate from "./pages/BoardCreate";
-import SignUp from "./pages/SignUp";
-import Tags from "./pages/Tags";
-import ArticleDetail from "./pages/ArticleDetail";
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Main from './pages/Main';
+import MainLayout from './components/layout/MainLayout';
+import BaseLayout from './components/layout/BaseLayout';
+import DrinksList from './pages/DrinksList';
+import DrinksDetail from './pages/DrinksDetail';
+import ArticleList from './pages/ArticleList';
+import MyPage from './pages/MyPage';
+import BoardList from './pages/BoardList';
+import BoardDetail from './pages/BoardDetail';
+import BoardCreate from './pages/BoardCreate';
+import SignUp from './pages/SignUp';
+import Tags from './pages/Tags';
+import ArticleDetail from './pages/ArticleDetail';
+import { useAppDispatch } from './redux/hooks/hooks';
+import { loginSuccess } from './redux/slice/auth/authSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  const initializeUserInfo = async () => {
+    try {
+      const res = await axios.get(`/members/mypage`);
+      if (res.status === 200) {
+        const userInfo = res.data;
+        dispatch(loginSuccess({ userInfo: userInfo }));
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    initializeUserInfo();
+  });
+
   return (
     <Routes>
       <Route element={<MainLayout img />}>
