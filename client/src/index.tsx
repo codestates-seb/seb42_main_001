b2bed89482492c924fe28082cd7eaa6c7b1326a2
hyunhoh <skins0329@gmail.com>
2023-03-21 11:00:22 +0900

    :sparkles: index.tsx에 store 경로 변경

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './global-style';
-import { store } from '../src/store/store';
+import { store } from './redux/store/store';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
