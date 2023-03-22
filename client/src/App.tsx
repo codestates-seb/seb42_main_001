cc3b2a6341e2394baee60eddc2736844494393d2
hyunhoh <skins0329@gmail.com>
2023-03-22 15:17:42 +0900

    :hammer: fix: App.tsx useEffect 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,25 +20,25 @@ import { loginSuccess } from './redux/slice/auth/authSlice';
 function App() {
   const dispatch = useAppDispatch();
 
-  const initializeUserInfo = async () => {
-    axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
-    axios.defaults.headers.common['Authorization'] =
-      localStorage.getItem('accessToken');
-    axios.defaults.headers.common['Refresh'] =
-      localStorage.getItem('refreshToken');
-
-    try {
-      const res = await axios.get(`/members/mypage`);
-      if (res.status === 200) {
-        const userInfo = res.data;
-        dispatch(loginSuccess({ userInfo: userInfo }));
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   useEffect(() => {
+    const initializeUserInfo = async () => {
+      axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+      axios.defaults.headers.common['Authorization'] =
+        localStorage.getItem('accessToken');
+      axios.defaults.headers.common['Refresh'] =
+        localStorage.getItem('refreshToken');
+
+      try {
+        const res = await axios.get(`/members/mypage`);
+        if (res.status === 200) {
+          const userInfo = res.data;
+          dispatch(loginSuccess({ userInfo: userInfo }));
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
     initializeUserInfo();
   });
 
