50a9a8f286a187cf7ca07dab163c97e5ab29e82c
hyunhoh <skins0329@gmail.com>
2023-03-22 21:52:49 +0900

    :sparkles: feat: MyPage Edit 기능 구현

--- client/src/components/MyPage/MyPageInfo.tsx
+++ client/src/components/MyPage/MyPageInfo.tsx
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
+
 import Card from '../UI/Card';
 import MyPageInfoContent from './MyPageInfoContent';
 import MyPageInfoItem from './MyPageInfoItem';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks/hooks';
+import {
+  modifyDisplayName,
+  modifyAboutMe,
+} from '../../redux/slice/auth/authSlice';
 
 function MyPageInfo() {
   const [userInfo, setUserInfo] = useState(false);
+  const { displayName, aboutMe } = useAppSelector(state => state.auth.userInfo);
+  const dispatch = useAppDispatch();
+
+  const handleDisplayNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(modifyDisplayName(e.target.value));
+  };
+
+  const handleAboutMeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(modifyAboutMe(e.target.value));
+  };
 
   return (
     <MarginContainer>
       <Card>
         <MainContainer>
-          <MyPageInfoContent userInfo={userInfo} />
+          <MyPageInfoContent
+            userInfo={userInfo}
+            handleDisplayNameChange={handleDisplayNameChange}
+            handleAboutMeChange={handleAboutMeChange}
+            displayName={displayName}
+            aboutMe={aboutMe}
+          />
           <MyPageInfoItem userInfo={userInfo} setUserInfo={setUserInfo} />
         </MainContainer>
       </Card>
