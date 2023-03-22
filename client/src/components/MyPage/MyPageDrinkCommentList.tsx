c6c780ac616d6c9b521f10ddf42d8411e29ef20d
hyunhoh <skins0329@gmail.com>
2023-03-22 11:33:32 +0900

    :sparkles: feat: MyPage Drink Comment List 렌더링

---
+++ client/src/components/MyPage/MyPageDrinkCommentList.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector } from '../../redux/hooks/hooks';
+import MyPageDrinkComment from './MyPageDrinkComment';
+
+function MyPageDrinkCommentList() {
+  const drinkCommentsList = useAppSelector(
+    state => state.auth.userInfo.writeDrinkComments,
+  );
+
+  return (
+    <>
+      {drinkCommentsList.map(ele => (
+        <MyPageDrinkComment key={ele.commentId} ele={ele}></MyPageDrinkComment>
+      ))}
+    </>
+  );
+}
+
+export default MyPageDrinkCommentList;
