46322db42e79bc6869d57ad71c17bbaf0c24f908
hyunhoh <skins0329@gmail.com>
2023-03-23 12:51:02 +0900

    :sparkles: feat: store에 tagReducer 연결

--- client/src/redux/store/store.ts
+++ client/src/redux/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slice/auth/authSlice';
+import tagReducer from '../slice/tag/tagSlice';
 
-export const store = configureStore({ reducer: { auth: authReducer } });
+export const store = configureStore({
+  reducer: { auth: authReducer, tag: tagReducer },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
