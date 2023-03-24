83e6df587e4b227bf92ed55522ba1de8f9ce913f
bibidi77 <fullmoon7813@gmail.com>
2023-03-24 17:31:18 +0900

    ✨ feat: redirec url 수정

--- Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/OAuth2/OAuth2MemberSuccessHandler.java
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/OAuth2/OAuth2MemberSuccessHandler.java
@@ -76,8 +76,7 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
 
         // UriComponentsBuilder를 사용하여 scheme+host+port+path 형식으로 구성한다.
         // ex: http://localhost:8080/mypage
-        return UriComponentsBuilder
-                .fromUriString("http;//localhost:3000/mypage")
+        return UriComponentsBuilder.fromUriString("http://localhost:3000/mypage")
                 .queryParam("Authorization", accessToken)
                 .queryParam("Refresh", refreshToken)
                 .build().toUriString();
