e8f8d42ecf47cea6bd001362537beb035b41b9ed
hyunhoh <skins0329@gmail.com>
2023-03-24 12:32:45 +0900

    :sparkles: feat: MyPage 리렌더링 적용

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -4,40 +4,57 @@ import styled from 'styled-components';
 
 import MyPageContent from '../components/MyPage/MyPageContent';
 import MyPageInfo from '../components/MyPage/MyPageInfo';
-import { useAppDispatch } from '../redux/hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../redux/hooks/hooks';
 import { loginSuccess } from '../redux/slice/auth/authSlice';
 
 function MyPage() {
   const dispatch = useAppDispatch();
+  const isLogin = useAppSelector(state => state.auth.isLogin);
 
   useEffect(() => {
-    const handleLoginProcess = async () => {
-      try {
-        const url = new URL(window.location.href);
-        const token = {
-          accessToken: `Bearer ${url.searchParams.get('Authorization')}`,
-          refreshToken: `${url.searchParams.get('Refresh')}`,
-        };
-        const res = await axios.get(`/members/mypage`, {
-          headers: {
-            Authorization: token.accessToken,
-            Refresh: token.refreshToken,
-          },
-        });
-        if (res.status === 200) {
-          const userInfo = res.data;
-          dispatch(loginSuccess({ userInfo: userInfo }));
-          localStorage.clear();
-          localStorage.setItem('accessToken', token.accessToken);
-          localStorage.setItem('refreshToken', token.refreshToken);
-          window.location.replace('/mypage');
+    if (!isLogin) {
+      const handleLoginProcess = async () => {
+        try {
+          const url = new URL(window.location.href);
+          const token = {
+            accessToken: `Bearer ${url.searchParams.get('Authorization')}`,
+            refreshToken: `${url.searchParams.get('Refresh')}`,
+          };
+          const res = await axios.get(`/members/mypage`, {
+            headers: {
+              Authorization: token.accessToken,
+              Refresh: token.refreshToken,
+            },
+          });
+          if (res.status === 200) {
+            const userInfo = res.data;
+            dispatch(loginSuccess({ userInfo: userInfo }));
+            localStorage.clear();
+            localStorage.setItem('accessToken', token.accessToken);
+            localStorage.setItem('refreshToken', token.refreshToken);
+            window.location.replace('/mypage');
+          }
+        } catch (e) {
+          console.error(e);
         }
-      } catch (e) {
-        console.error(e);
-      }
-    };
+      };
 
-    handleLoginProcess();
+      handleLoginProcess();
+    } else {
+      const getUserInfo = async () => {
+        try {
+          const res = await axios.get(`/members/mypage`);
+          if (res.status === 200) {
+            const userInfo = res.data;
+            dispatch(loginSuccess({ userInfo: userInfo }));
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
+      getUserInfo();
+    }
   });
 
   return (
