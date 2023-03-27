4a98a7073bc6732a1b31d744baa6093f4a50bbdb
Dvdhan <han2041126@gmail.com>
2023-03-27 11:38:57 +0900

    up

--- Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/Config/ComponentConfiguration.java
+++ Server_001_WithsKey/src/main/java/com/codestates/server_001_withskey/global/security/Config/ComponentConfiguration.java
@@ -3,8 +3,11 @@ package com.codestates.server_001_withskey.global.security;
 import com.codestates.server_001_withskey.global.security.Redis.TokenRedisRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-@Configuration
+import org.springframework.stereotype.Component;
+
+@Component
 public class ComponentConfiguration {
     @Bean
     public TokenRedisRepository tokenRedisRepository(StringRedisTemplate stringRedisTemplate) {
