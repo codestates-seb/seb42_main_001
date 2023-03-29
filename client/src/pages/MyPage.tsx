c30c118c8ce31c3a1dff7ceb793f0910fef309f5
hyunhoh <skins0329@gmail.com>
2023-03-30 02:18:54 +0900

    :recycle: refactor: MyPage 로그인 처리 함수 수정

--- client/src/pages/MyPage.tsx
+++ client/src/pages/MyPage.tsx
@@ -1,61 +1,42 @@
-import axios from 'axios';
 import { useEffect } from 'react';
 import styled from 'styled-components';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 import MyPageContent from '../components/MyPage/MyPageContent';
 import MyPageInfo from '../components/MyPage/MyPageInfo';
-import { useAppDispatch, useAppSelector } from '../redux/hooks/hooks';
+import { useAppDispatch } from '../redux/hooks/hooks';
 import { loginSuccess } from '../redux/slice/auth/authSlice';
+import customAxios from '../api/customAxios';
 
 function MyPage() {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const isLogin = useAppSelector(state => state.auth.isLogin);
 
   useEffect(() => {
-    if (!isLogin) {
+    if (searchParams.get('Authorization')) {
       const handleLoginProcess = async () => {
         try {
-          const url = new URL(window.location.href);
           const token = {
-            accessToken: `Bearer ${url.searchParams.get('Authorization')}`,
-            refreshToken: `${url.searchParams.get('Refresh')}`,
+            accessToken: `Bearer ${searchParams.get('Authorization')}`,
+            refreshToken: `${searchParams.get('Refresh')}`,
           };
-          const res = await axios.get(`/members/mypage`, {
-            headers: {
-              Authorization: token.accessToken,
-              Refresh: token.refreshToken,
-            },
-          });
-          if (res.status === 200) {
-            const userInfo = res.data;
-            dispatch(loginSuccess({ userInfo: userInfo }));
-            localStorage.clear();
-            localStorage.setItem('accessToken', token.accessToken);
-            localStorage.setItem('refreshToken', token.refreshToken);
-            window.location.replace('/mypage');
-          }
+
+          localStorage.setItem('accessToken', token.accessToken);
+          localStorage.setItem('refreshToken', token.refreshToken);
+
+          const res = await customAxios.get('/members/mypage');
+
+          dispatch(loginSuccess({ userInfo: res.data }));
+          navigate('/mypage');
         } catch (e) {
           console.error(e);
         }
       };
 
       handleLoginProcess();
-    } else {
-      const getUserInfo = async () => {
-        try {
-          const res = await axios.get(`/members/mypage`);
-          if (res.status === 200) {
-            const userInfo = res.data;
-            dispatch(loginSuccess({ userInfo: userInfo }));
-          }
-        } catch (e) {
-          console.error(e);
-        }
-      };
-
-      getUserInfo();
     }
-  });
+  }, [dispatch, navigate, searchParams]);
 
   return (
     <MainContainer>
