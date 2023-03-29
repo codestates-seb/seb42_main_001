8b593bed2f31cea16d5c8d3f54e68356f93331e7
hyunhoh <skins0329@gmail.com>
2023-03-29 10:34:46 +0900

    :lipstick: design: SignUp 페이지 게스트 로그인 모드 디자인 추가

--- client/src/components/SignUp/SignUpButton.tsx
+++ client/src/components/SignUp/SignUpButton.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
-import { FcGoogle } from 'react-icons/fc';
 
-const SignUpButton = () => {
+import { FcGoogle } from 'react-icons/fc';
+import { FaUserCircle } from 'react-icons/fa';
+
+interface props {
+  loginMode: string;
+}
+
+const SignUpButton = ({ loginMode }: props) => {
   const handleLogin = () => {
     window.location.assign(
       `${process.env.REACT_APP_BASE_URL}/oauth2/authorization/google`,
@@ -10,8 +16,12 @@ const SignUpButton = () => {
 
   return (
     <SignUpButtonContainer onClick={handleLogin}>
-      <FcGoogle size="40" />
-      Google로 시작하기
+      {loginMode === 'Google' ? (
+        <FcGoogle size="40" />
+      ) : (
+        <FaUserCircle size="40" />
+      )}
+      {`${loginMode}로 시작하기`}
     </SignUpButtonContainer>
   );
 };
@@ -21,18 +31,25 @@ export default SignUpButton;
 const SignUpButtonContainer = styled.button`
   width: 80%;
   height: 90px;
+  margin: var(--x-large) 0;
   background-color: var(--color-white);
   border: 1px solid var(--color-sub-gray);
   border-radius: var(--2x-small);
-  margin-bottom: 150px;
   display: flex;
   align-items: center;
   font-size: var(--text-medium);
   font-weight: var(--weight-large);
   cursor: pointer;
+  transition: 0.4s;
+
   > svg {
     margin-left: var(--medium);
     margin-right: 140px;
+  }
+
+  &:hover {
+    transition: 0.4s;
+    box-shadow: 0px 0px 20px #473f3f73;
   }
 
   @media only screen and (max-width: 768px) {
