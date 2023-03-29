d9f99688a661dda86cb2512213b602c785a91a02
lapmu <antjdgkdl@gmail.com>
2023-03-29 19:44:06 +0900

    🐛 fix: Board Detail recommend 내용 변경 불가 에러 수정

--- client/src/components/Board/BoardDetailContents.tsx
+++ client/src/components/Board/BoardDetailContents.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-import { Viewer } from '@toast-ui/react-editor';
 import { useAppSelector } from '../../redux/hooks/hooks';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function BoardDetailContents() {
   const { content } = useAppSelector((state) => state.boardDetail.detailData);
 
   return (
     <ContentsContainer>
-      <Viewer initialValue={content || ''} />
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={[remarkGfm]}
+      ></ReactMarkdown>
     </ContentsContainer>
   );
 }
@@ -19,6 +23,9 @@ const ContentsContainer = styled.div`
   font-size: var(--text-medium);
   line-height: var(--large);
   margin-bottom: var(--4x-large);
+  img {
+    max-width: 100%;
+  }
 
   @media only screen and (max-width: 768px) {
     margin-bottom: var(--large);
