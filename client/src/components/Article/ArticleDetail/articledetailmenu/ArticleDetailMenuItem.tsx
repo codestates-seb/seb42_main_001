08ecda62724f7a3c9233745c6b57c293d8d1f994
dding-v <v.dding.v@gmail.com>
2023-03-30 19:10:41 +0900

    :lipstick: design: Article Detail Menu 클릭 시 배경색 고정

--- client/src/components/Article/ArticleDetail/articledetailmenu/ArticleDetailMenuItem.tsx
+++ client/src/components/Article/ArticleDetail/articledetailmenu/ArticleDetailMenuItem.tsx
@@ -1,3 +1,4 @@
+// import { useState } from 'react';
 import styled from 'styled-components';
 import Card from '../../../UI/Card';
 
@@ -5,28 +6,38 @@ interface MenuItemProps {
   title: string;
   value: number;
   handlIdChange: (value: number) => void;
+  isActive: number;
 }
 
-function ArticleDetailMenuItem({ title, value, handlIdChange }: MenuItemProps) {
+function ArticleDetailMenuItem({ title, value, handlIdChange, isActive }: MenuItemProps) {
+  // const [isActive, setIsActive] = useState<boolean>(false);
+
+  // const handleClick = () => {
+  //   setIsActive(prev => !prev);
+  //   handlIdChange(value);
+  //   console.log('zz')
+  // };
+
   return (
-    <MainContainer>
+    <MainContainer isActive={isActive}>
       <Card>
-        <SizeContainer onClick={() => handlIdChange(value)}>
+        {/* <SizeContainer isActive={isActive} onClick={() => handlIdChange(value)}> */}
+        <SizeContainer isActive={isActive === value} onClick={() => handlIdChange(value)}>
           {title}
         </SizeContainer>
       </Card>
-    </MainContainer>
+    </MainContainer >
   );
 }
 
 export default ArticleDetailMenuItem;
 
-const MainContainer = styled.div`
+const MainContainer = styled.div<{ isActive: number }>`
   width: 100%;
   height: 100%;
   margin-bottom: var(--small);
   color: var(--color-main);
-  opacity: 0.6;
+  opacity: 1;
   font-weight: var(--weight-small);
   transition: 0.4s;
   cursor: pointer;
@@ -41,11 +52,13 @@ const MainContainer = styled.div`
   }
 `;
 
-const SizeContainer = styled.button`
+const SizeContainer = styled.button<{ isActive: any }>`
   cursor: pointer;
   width: 100%;
   display: flex;
   border: 0;
-  background-color: transparent;
+  border-radius: var(--3x-small);
+  background-color: ${(props) => (props.isActive ? `var(--color-white)` : `var(--color-sub-dark-gray)`)};
   padding: var(--medium);
+  transition: 0.4s;
 `;
