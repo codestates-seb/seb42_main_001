73dd9af160e234cc2ce765b2b2171cbbbd9f912e
dding-v <v.dding.v@gmail.com>
2023-04-17 19:26:16 +0900

    :fire: chore: 불필요한 코드 삭제

--- client/src/components/drinks/drinksDetail/drinksDetailContent/drinksDetailSnacks/DrinksDetailSnack.tsx
+++ client/src/components/drinks/drinksDetail/drinksDetailContent/drinksDetailSnacks/DrinksDetailSnack.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import Card from "../../../../UI/Card";
 import { IDrinksSnacksProps } from '../../../../../util/interfaces/drinks.inerface'
